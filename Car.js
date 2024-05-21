72797c92f309dbd9fd66c9966d06df062d2338b8
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-20 21:24:45 -0400

    Add break method

--- Car.js
+++ Car.js
@@ -13,6 +13,15 @@ class Car {
 
     }//END
 
+// Implementing brake method that decreases car speed
+brake() {
+    this.speedValue=this.speedValue-5;
+    console.log("Car model:"+this.makeName);
+    console.log("Reduced speed:" +this.speedValue);  
+
+    
+}
+
 
 
 }
