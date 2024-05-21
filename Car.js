b46dce62c54a458ff6546d4883c0e8d5ad8b7f69
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-20 21:30:13 -0400

    Added objects for cars

--- Car.js
+++ Car.js
@@ -1,35 +1,37 @@
 class Car {
     //constructor
-    constructor(make,speed) {  
-        this.makeName=make;
-        this.speedValue=speed;
+    constructor(make, speed) {
+        this.makeName = make;
+        this.speedValue = speed;
     }
-
-     // implementing accelerate method to increase speed by 10 km
+    // implementing accelerate method to increase speed by 10 km
     accelerate() {
-        this.speedValue= this.speedValue+10;
-        console.log("Car model:"+ this.makeName);
+        this.speedValue = this.speedValue + 10;
+        console.log("Car model:" + this.makeName);
         console.log("New speed: " + this.speedValue);
 
     }//END
 
-// Implementing brake method that decreases car speed
-brake() {
-    this.speedValue=this.speedValue-5;
-    console.log("Car model:"+this.makeName);
-    console.log("Reduced speed:" +this.speedValue);  
-
-    
+    // Implementing brake method that decreases car speed
+    brake() {
+        this.speedValue = this.speedValue - 5;
+        console.log("Car model:" + this.makeName);
+        console.log("Reduced speed:" + this.speedValue);
+    }
 }
+// Creating and testing specific car objects
+myBMW = new Car("BMW", 120);
+myBMW.accelerate();
+myBMW.brake();
+
+myMercedes = new Car("Mercedes", 95);
+myMercedes.accelerate();
+myMercedes.brake();
 
 
 
-}
 
 
 
 
 
-    
-
-    
